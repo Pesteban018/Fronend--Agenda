7f25278e205eaac570bbf381b1fd56042128d0dc
Pesteban018 <e9293498569@gmail.com>
2023-12-05 14:55:11 -0400

    actualizacion calendario

--- src/page/Calendario.jsx
+++ src/page/Calendario.jsx
@@ -1,4 +1,3 @@
-// CalendarComponent.jsx
 import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -6,21 +5,77 @@ import 'moment/locale/es';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import Modal from 'react-modal';
 import { useTasks } from '../context/TasksContext';
+import Toolbar from 'react-big-calendar/lib/Toolbar';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // Importa los iconos según tu elección
 
 moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
 Modal.setAppElement('#root');
+class CustomToolbar extends Toolbar {
+  render() {
+    const { localizer: { messages }, onNavigate, label } = this.props;
+
+    return (
+      <div className="rbc-toolbar">
+        <span className="rbc-btn-group">
+          <button type="button" onClick={() => onNavigate('PREV')}>
+            <FaChevronLeft />
+          </button>
+          <button type="button" onClick={() => onNavigate('TODAY')}>
+            {messages.today}
+          </button>
+          <button type="button" onClick={() => onNavigate('NEXT')}>
+            <FaChevronRight />
+          </button>
+        </span>
+        <span className="rbc-toolbar-label">{label}</span>
+      </div>
+    );
+  }
+}
+
+const messages = {
+  allDay: 'Todo el día',
+  previous: 'Anterior',
+  next: 'Siguiente',
+  today: 'Hoy',
+  month: 'Mes',
+  week: 'Semana',
+  day: 'Día',
+  agenda: 'Agenda',
+  date: 'Fecha',
+  time: 'Hora',
+  event: 'Evento',
+  showMore: total => `+ Ver más (${total})`, 
+  noEventsInRange: 'No hay eventos en este rango',  
+};
+
+moment.updateLocale('es', {
+  weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+  months: [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre',
+  ],
+  monthsShort: [
+    'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+    'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic',
+  ],
+});
 
 const CalendarComponent = ({ isNavVisible }) => {
   const { tasks, getTasks } = useTasks();
 
   useEffect(() => {
-    getTasks(); // Llama a getTasks después de montar el componente
+    const fetchData = async () => {
+      await getTasks();
+    };
+
+    fetchData(); // Llama a getTasks después de montar el componente
   }, []);
 
-  console.log(tasks);
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
 
@@ -43,8 +98,6 @@ const CalendarComponent = ({ isNavVisible }) => {
         end: new Date(task.date),
       }));
 
-      console.log(taskEvents);
-
       setEvents(taskEvents);
       localStorage.setItem('calendarEvents', JSON.stringify(taskEvents));
     }
@@ -53,12 +106,10 @@ const CalendarComponent = ({ isNavVisible }) => {
   const handleSelectEvent = (event) => {
     setSelectedEvent(event);
   };
-
   return (
     <div className={`fixed bg-white ${isNavVisible ? 'right-[0%] left-[1%] relative' : ' relative left-[-24%] -mr-44'} transition-all duration-300 ease-in-out h-max mr-3 rounded-lg`}>
       <div className='ml-[2%] mr-10 w-auto items-center justify-between absol mt-12 bg'>
-    
-      <Calendar
+        <Calendar
           localizer={localizer}
           events={events}
           startAccessor="start"
@@ -66,11 +117,14 @@ const CalendarComponent = ({ isNavVisible }) => {
           onSelectEvent={handleSelectEvent}
           onSelecting={() => false}
           style={{ height: '500px' }}
-          views={['month', 'week', 'day']}
           formats={{
             eventTimeRangeFormat: ({ start, end }, culture, local) =>
               `${moment(start).format('LT')} - ${moment(end).format('LT')}`,
           }}
+          components={{
+            toolbar: CustomToolbar,
+          }}
+          messages={messages}  // Agregar los mensajes personalizados aquí
         />
         {selectedEvent && (
           <Modal
