164a12cedb37445e6497fb57eda87af32f8d9a9c
Pesteban018 <e9293498569@gmail.com>
2023-12-05 11:43:59 -0400

    actualizacion task

--- src/page/card.jsx
+++ src/page/card.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import notify from "../context/notify";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import {
   getTasksRequest,
   createTaskRequest,
@@ -22,7 +22,7 @@ import {
 import { MdDeleteForever, MdAddTask } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
 import { IoEye } from "react-icons/io5";
-import moment from "moment"; 
+import moment from "moment";
 class App extends Component {
   state = {
     tasks: [],
@@ -36,8 +36,9 @@ class App extends Component {
       user: "",
     },
     tareaEliminar: null,
-    selectedEvent: null, 
-    maxDescriptionLength: 30, 
+    selectedEvent: null,
+    maxDescriptionLength: 30,
+    maxTitleLength: 15,
   };
 
   componentDidMount() {
@@ -71,7 +72,7 @@ class App extends Component {
   eliminar = () => {
     const { tareaEliminar } = this.state;
     console.log("Deleting task with ID:", tareaEliminar._id);
-  
+
     deleteTaskRequest(tareaEliminar._id)
       .then(() => {
         console.log("Task deleted successfully");
@@ -86,29 +87,38 @@ class App extends Component {
         console.error("Error al eliminar tarea:", error);
       });
   };
-  
-  
 
   render() {
-    const { tasks, modalEliminar, tareaEliminar, selectedEvent, maxDescriptionLength } = this.state;
+    const {
+      tasks,
+      modalEliminar,
+      tareaEliminar,
+      selectedEvent,
+      maxDescriptionLength,
+      maxTitleLength,
+    } = this.state;
     const { isNavVisible } = this.props;
-    const dynamicMaxDescriptionLength = isNavVisible ? 10 : maxDescriptionLength;
+    const dynamicMaxDescriptionLength = isNavVisible
+      ? 10
+      : maxDescriptionLength;
+    const dynamicMaxtitleLength = isNavVisible ? 10 : maxTitleLength;
 
     return (
       <>
-       <div className={`fixed h-max bg-white top-12 ${isNavVisible ? 'right-[0%] left-[26%]' : 'left-[7%] right-0'} transition-all duration-300 ease-in-out h-max mr-3 rounded-lg`}>
-     
-        <ToastContainer />
- 
-        
-            <Link to="/add-task">
-              <Button className="bg-green-500 ml-2 hover:bg-green-700 flex space-x-2 items-center text-white font-bold rounded mb-3 mt-7">
-                <MdAddTask /> <span> Nueva Tarea</span>
-              </Button>
-            </Link>
+        <div
+          className={`fixed h-max bg-white top-12 ${
+            isNavVisible ? "right-[0%] left-[26%]" : "left-[7%] right-0"
+          } transition-all duration-300 ease-in-out h-max mr-3 rounded-lg`}
+        >
+          <ToastContainer />
 
-            <Container>
-         
+          <Link to="/add-task">
+            <Button className="bg-green-500 ml-2 hover:bg-green-700 flex space-x-2 items-center text-white font-bold rounded mb-3 mt-7">
+              <MdAddTask /> <span> Nueva Tarea</span>
+            </Button>
+          </Link>
+
+          <Container>
             <Table responsive className="relative ml-auto mr-auto h-44">
               <thead>
                 <tr>
@@ -119,50 +129,71 @@ class App extends Component {
                 </tr>
               </thead>
               {tasks.length === 0 ? (
-            <div className=" absolute left-60 mt-14">
-              <h1 className=" mb-2">NO SE ENCUENTRAN TAREAS AGREGADAS...</h1>
-             
-            </div>
-          ) : (
-              <tbody>
-                
-                {tasks.map((elemento) => (
-                  <tr key={elemento._id}>
-                    <td>{elemento.title.substring(0, 15)}</td>
-                <td className={`max-description-length transition-all duration-300 ease-in-out ${isNavVisible ? 'max-w-[100px]' : 'max-w-[300px]'}`}>
-                  {elemento.description.length > dynamicMaxDescriptionLength
-                    ? `${elemento.description.substring(0, dynamicMaxDescriptionLength)}...`
-                    : elemento.description}
-                </td>
-                <td>{new Date(elemento.date).toLocaleDateString()}</td>
-           
-                    <td>
-                      <Link
-                        to={`/tasks/${elemento._id}`}
-                        className="btn btn-primary mr-2"
+                <div className=" absolute left-60 mt-14">
+                  <h1 className=" mb-2">
+                    NO SE ENCUENTRAN TAREAS AGREGADAS...
+                  </h1>
+                </div>
+              ) : (
+                <tbody>
+                  {tasks.map((elemento) => (
+                    <tr key={elemento._id}>
+                      <td
+                        className={`max-title-length transition-all duration-300 ease-in-out ${
+                          isNavVisible ? "max-w-[100px]" : "max-w-[300px]"
+                        }`}
                       >
-                        <CiEdit />
-                      </Link>
-                      <button
-                        className="btn btn-danger mr-2"
-                        onClick={() => this.mostrarModalEliminar(elemento)}
+                        {elemento.title.length > dynamicMaxtitleLength
+                          ? `${elemento.title.substring(
+                              0,
+                              dynamicMaxtitleLength
+                            )}...`
+                          : elemento.title}
+                      </td>
+                      <td
+                        className={`max-description-length transition-all duration-300 ease-in-out ${
+                          isNavVisible ? "max-w-[100px]" : "max-w-[300px]"
+                        }`}
                       >
-                        <MdDeleteForever />
-                      </button>
-                      <button
-                        className="btn btn-info "
-                        onClick={() => this.setState({ selectedEvent: elemento })}
-                      >
-                        <IoEye />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>)}
-            </Table> 
+                        {elemento.description.length >
+                        dynamicMaxDescriptionLength
+                          ? `${elemento.description.substring(
+                              0,
+                              dynamicMaxDescriptionLength
+                            )}...`
+                          : elemento.description}
+                      </td>
+                      <td>{new Date(elemento.date).toLocaleDateString()}</td>
+
+                      <td>
+                        <Link
+                          to={`/tasks/${elemento._id}`}
+                          className="btn btn-primary mr-2"
+                        >
+                          <CiEdit />
+                        </Link>
+                        <button
+                          className="btn btn-danger mr-2"
+                          onClick={() => this.mostrarModalEliminar(elemento)}
+                        >
+                          <MdDeleteForever />
+                        </button>
+                        <button
+                          className="btn btn-info "
+                          onClick={() =>
+                            this.setState({ selectedEvent: elemento })
+                          }
+                        >
+                          <IoEye />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </Table>
           </Container>
 
-     
           <Modal isOpen={modalEliminar}>
             <ModalHeader>
               <div>
@@ -173,7 +204,9 @@ class App extends Component {
             <ModalBody>
               <h2 className="whitespace-normal overflow-auto">
                 ¿Estás seguro de que deseas eliminar la tarea con el Nombre:{" "}
-               <h2 className=" font-bold">{tareaEliminar ? tareaEliminar.title : ""}?</h2> 
+                <h2 className=" font-bold">
+                  {tareaEliminar ? tareaEliminar.title : ""}?
+                </h2>
               </h2>
             </ModalBody>
 
@@ -216,22 +249,26 @@ class App extends Component {
                   <h2 className=" text-lg font-bold mb-1 text-black">
                     Descripción:
                   </h2>{" "}
-                  <h2 className="text-lg text-black">{selectedEvent?.description}</h2>
+                  <h2 className="text-lg text-black">
+                    {selectedEvent?.description}
+                  </h2>
                 </h1>
                 <h1 className="text-gray-700 mb-4 mr-2">
-                <div style={{ display: "flex", marginBottom: "1rem" }}>
-                  <h2 className="text-lg font-bold mb-1 text-black">Fecha:</h2>
-                  <h2 className="text-lg text-black">
-                    {moment(selectedEvent?.start).format("LLL")}
-                  </h2>
+                  <div style={{ display: "flex", marginBottom: "1rem" }}>
+                    <h2 className="text-lg font-bold mb-1 text-black">
+                      Fecha:
+                    </h2>
+                    <h2 className="text-lg text-black">
+                      {moment(selectedEvent?.start).format("LLL")}
+                    </h2>
                   </div>
                 </h1>
                 <h1 className="text-gray-700 mb-4 mr-2">
-                <div style={{ display: "flex", marginBottom: "1rem" }}>
-                  <h2 className="text-lg font-bold mb-1 text-black">Hora:</h2>
-                  <h2 className="text-lg text-black">
-                    {moment(selectedEvent?.start).format("LT")}
-                  </h2>
+                  <div style={{ display: "flex", marginBottom: "1rem" }}>
+                    <h2 className="text-lg font-bold mb-1 text-black">Hora:</h2>
+                    <h2 className="text-lg text-black">
+                      {moment(selectedEvent?.start).format("LT")}
+                    </h2>
                   </div>
                 </h1>
                 <button
@@ -243,10 +280,10 @@ class App extends Component {
               </div>
             </Modal>
           )}
-           </div>
-        </>
-      );
-    }
+        </div>
+      </>
+    );
   }
+}
 
-  export default App;
+export default App;
